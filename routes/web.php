24b56b2755b83036be7142b7197f835a29f71f1e
root <root@ip-172-30-1-91.eu-west-1.compute.internal>
2017-03-14 14:20:52 -0400

    gencodes route

--- routes/web.php
+++ routes/web.php
@@ -19,4 +19,26 @@ Route::get('redeem', 'DownloadController@redeem');
 
 Route::post('checkcode', 'DownloadController@check');
 
+Route::get('gencodes', function() {
+
+dd('disabled');
+
+function randomKey($length) {
+    $pool = array_merge(range(0,9), range('a', 'z'),range('A', 'Z'));
+    $key = '';
+    for($i=0; $i < $length; $i++) {
+        $key .= $pool[mt_rand(0, count($pool) - 1)];
+    }
+    return $key;
+}
+
+	for ($i = 0; $i < 600; $i++) {
+		$code = 'DALL' . randomKey(12);
+
+		$c = new \App\Code;
+		$c->code = $code;
+		$c->save();
+	}
+});
+
 // Route::get('download', 'DownloadController@download');
